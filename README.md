2fa6e47b41eec8d0bb24f51a832fd993dc87a68c
Radek Šerý <radek.sery@peckadesign.cz>
2020-08-31 14:00:19 +0200

    Version 0.1.0 of chrome extension for dynamic filtering github timeline

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# github-timeline-filter
+## Github Timeline Filter
+
+### Installation
+
+1. Clone the repository
+2. Go to the extensions: `chrome://extensions/`
+3. Enable developer mode (in the top right corner)
+4. Select cloned folder by using `LOAD UNPACKED`
---
+++ userscript.js
@@ -0,0 +1,146 @@
+(function (window) {
+
+	let labels = {
+		comments: 'Comments',
+		commits: 'Commits',
+		pulls: 'Cross reference in pull request',
+		issues: 'Cross reference in issues',
+		others: 'Other (labels, assignment, projects, …)'
+	};
+
+	let items = {
+		comments: null,
+		commits: null,
+		pulls: null,
+		issues: null,
+		others: null
+	};
+
+	let show = {
+		comments: true,
+		commits: true,
+		pulls: true,
+		issues: true,
+		others: true
+	};
+
+	let storage = chrome.storage.sync;
+
+	function initExtension() {
+		storage.get(['gtfShow'], result => {
+			if (result.gtfShow) {
+				Object.assign(show, result.gtfShow);
+			}
+
+			/* Take care of AJAX pagination */
+			const targetNode = document.querySelector('.js-issue-timeline-container');
+			const config = { attributes: false, childList: true, subtree: true };
+			const observer = new MutationObserver(function (mutationsList) {
+				updateTimelineItems();
+				filterTimeline();
+			});
+			observer.observe(targetNode, config);
+
+			/* Initialize */
+			createFilter();
+			updateTimelineItems();
+			filterTimeline();
+		});
+	}
+
+
+	function toggleCheckbox() {
+		show[this.dataset.type] = this.checked;
+
+		storage.set({ 'gtfShow': show });
+
+		filterTimeline();
+	}
+
+	function createFilter() {
+		let div = document.createElement('div');
+		div.classList.add('gtf');
+
+		for (type in show) {
+			if (show.hasOwnProperty(type)) {
+				let label = document.createElement('label');
+				label.setAttribute('for', 'gtf-' + type);
+				label.innerText = type[0].toUpperCase() + type.slice(1);
+				label.title = labels[type];
+				label.classList.add('gtf__item');
+
+				let checkbox = document.createElement('input');
+				checkbox.type = 'checkbox';
+				checkbox.id = 'gtf-' + type;
+				checkbox.name = 'gtf';
+				checkbox.classList.add('gtf__checkbox');
+				checkbox.dataset.type = type;
+				checkbox.checked = show[type];
+				checkbox.addEventListener('change', toggleCheckbox);
+
+				label.prepend(checkbox);
+				div.append(label);
+			}
+		}
+
+		document.body.append(div);
+	}
+
+	function updateTimelineItems() {
+		let timelineItems = document.querySelectorAll('.TimelineItem');
+		timelineItems = Array.from(timelineItems);
+
+		items.comments = timelineItems.filter(item => {
+			return item.classList.contains('js-comment-container');
+		});
+
+		items.commits = timelineItems.filter(item => {
+			return item.querySelector('.TimelineItem-body[id^="ref-commit-"]');
+		});
+
+		items.pulls = timelineItems.filter(item => {
+			return item.querySelector('[data-hovercard-type="pull_request"]') &&
+				! item.classList.contains('js-comment-container') &&
+				! item.querySelector('.TimelineItem-body[id^="ref-commit-"]');
+		});
+
+		items.issues = timelineItems.filter(item => {
+			return item.querySelector('[data-hovercard-type="issue"]') &&
+				! item.classList.contains('js-comment-container') &&
+				! item.querySelector('.TimelineItem-body[id^="ref-commit-"]');
+		});
+
+		items.others = timelineItems.filter(item => {
+			return items.comments.indexOf(item) === -1 && items.commits.indexOf(item) === -1 &&
+				items.pulls.indexOf(item) === -1 && items.issues.indexOf(item) === -1;
+		});
+	}
+
+	function filterTimeline() {
+		for (type in show) {
+			if (items.hasOwnProperty(type)) {
+				items[type].forEach(item => {
+					if (! show[type]) {
+						// console.log("Hide:", item, type);
+						item.classList.add('gtf-hidden');
+					} else {
+						// console.log("Show:", item, type);
+						item.classList.remove('gtf-hidden');
+					}
+				});
+			}
+		}
+	}
+
+
+
+	if (document.readyState === 'loading') {
+		document.addEventListener("DOMContentLoaded", function(e) {
+			initExtension();
+		});
+	}
+	else {
+		initExtension();
+	}
+
+})(window);
